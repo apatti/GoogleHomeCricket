a9c9cbb8a09c5b0b675c3d57c654f7f02d06fae6
Ashwin Patti <ashwin.patti@gmail.com>
2016-12-22 03:33:16 -0800

    get team summary

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.get('/matches/:team/summary',function(req,res){
 
 app.post('/googlehome/matches',function(req,res){
   controller.allGames(function(rss) {
-    res.send({speech:rss[0],displayText:rss[0],data:[],contextOut:[],source:""});
+    res.send({speech:rss[0],displayText:rss[0],data:{},contextOut:[],source:""});
   });
 })
 
