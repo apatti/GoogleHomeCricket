9a9f017f768de6452921154878b6f6120bb80c39
Ashwin Patti <ashwin.patti@gmail.com>
2016-12-24 00:20:31 -0800

    google game summary

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json({type: 'application/json'}));
 const SUMMARY_INTENT = 'gamesummary';
 const MATCHES_INTENT = 'matches';
 const IS_TEAM_PLAYING_INTENT = 'isteamplaying';
+const scoreRunsRegex = /([0-9]+)((\/)?([0-9]+)?( )?\(?([0-9.]+)?( ov\))?)?/g;
 
 app.get('/',function(req,res){
   res.send('Hello google home!!');
@@ -55,21 +56,26 @@ app.post('/',function(req,res){
         text_speech += '<speak>Sorry, '+assistant.getArgument('team')+' is not playing any game now <break time="1s"/> </speak>'
       }
       else {
-
-        text_speech += '<p><s>'+summaryObj.summary+'</s><break time="1s"/>';
-        if(summaryObj.team1.score!=="")
-        {
-          text_speech += '<s>'+summaryObj.team1.name+' score is '+summaryObj.team1.score+'</s><break time="1s"/>';
-        }
-        if(summaryObj.team2.score!=="")
-        {
-          text_speech += '<s>and '+summaryObj.team2.name+' score is '+summaryObj.team2.score+'</s><break time="1s"/>';
-        }
+        text_speech += getSummarySpeech(summaryObj);
       }
       text_speech += '</speak>';
       assistant.tell(text_speech);
 
     });
+  }
+
+  function getSummarySpeech(summaryObj)
+  {
+    text_speech += '<p><s>'+summaryObj.summary+'</s><break time="1s"/>';
+    if(summaryObj.team1.score!=="")
+    {
+      text_speech += '<s>'+summaryObj.team1.name+' score is '+summaryObj.team1.score+'</s><break time="1s"/>';
+    }
+    if(summaryObj.team2.score!=="")
+    {
+      text_speech += '<s>and '+summaryObj.team2.name+' score is '+summaryObj.team2.score+'</s><break time="1s"/>';
+    }
+    text_speech='</p>';
   }
 
   function getScoreInSpeech(score)
@@ -78,7 +84,57 @@ app.post('/',function(req,res){
     if(score.indexOf('&'))
     {
       scoreList = score.split('&');
+      for(var teamScore of scoreList)
+      {
+        let teamScoreSpeech = getRunsWicketsInSpeech(teamScore);
+        if(teamScoreSpeech==null)
+        {
+          return null;
+        }
+        scoreSpeech+=teamScoreSpeech;
+      }
     }
+    else {
+      scoreSpeech = getRunsWicketsInSpeech(score);
+    }
+    return scoreSpeech;
+  }
+
+  function getRunsWicketsInSpeech(score)
+  {
+    var speech = '';
+    let scoreRegexMatches = scoreRunsRegex.exec(score);
+    // index 1 is runs.
+    // index 4 is wickets
+    // index 6 is overs
+    if(scoreRegexMatches==null)
+    {
+      return null;
+    }
+    speech = '<s>'+score[1] '<break time="500ms"/>';
+
+    if(scoreRegexMatches[4]!=null)
+    {
+      if(scoreRegexMatches[4]==0)
+      {
+        speech += ' no loss <break time="500ms"/>';
+      }
+      else if(scoreRegexMatches[4]==10)
+      {
+        speech += ' all out <break time="500ms"/>';
+      }
+      else
+      {
+        speech += ' for loss of '+scoreRegexMatches[4]+' wickets <break time="500ms"/>';
+      }
+    }
+
+    if(scoreRegexMatches[6]!=null)
+    {
+      speech += ' in '+scoreRegexMatches[6]+' overs.</s><break time="1s"/>'
+    }
+
+    return speech;
   }
 
   function isTeamPlaying(assistant)
@@ -92,20 +148,10 @@ app.post('/',function(req,res){
         text_speech += 'Sorry, '+team+' is not playing any game now <break time="1s"/>'
       }
       else {
-
-        text_speech += '<p><s>Yes, the '+team+' is playing <break time="500ms"/> and the match summary is:</s><s>'+summaryObj.summary+'. </s><break time="1s"/>';
-        if(summaryObj.team1.score!=="")
-        {
-          text_speech += '<s>'+summaryObj.team1.name+' score is '+summaryObj.team1.score+'</s><break time="1s"/>';
-        }
-        if(summaryObj.team2.score!=="")
-        {
-          text_speech += '<s>and '+summaryObj.team2.name+' score is '+summaryObj.team2.score+'</s><break time="1s"/>';
-        }
+        text_speech += '<s>Yes, the '+team+' is playing <break time="500ms"/> and the match summary is:</s><s>'+getSummarySpeech(summaryObj);
       }
       text_speech += '</speak>';
       assistant.tell(text_speech);
-
     });
   }
 
