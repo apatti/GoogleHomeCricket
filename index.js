80ddce8d20dd87d808a3636d1790b88c870910a7
Ashwin Patti <ashwin.patti@gmail.com>
2016-12-24 01:31:57 -0800

    google game summary

--- index.js
+++ index.js
@@ -56,9 +56,8 @@ app.post('/',function(req,res){
         text_speech += '<speak>Sorry, '+assistant.getArgument('team')+' is not playing any game now <break time="1s"/> </speak>'
       }
       else {
-        assistant.tell("Hello "+assistant.getArgument('team'));
-        return;
         text_speech += getSummarySpeech(summaryObj);
+        assistant.tell(text_speech);
         console.log(text_speech);
       }
       text_speech += '</speak>';
